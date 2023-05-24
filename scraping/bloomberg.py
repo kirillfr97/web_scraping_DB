17e89a351246659eb796f9b9ec07400784100111
Кирилл В Фрейдин <frejdin.kirill-v@net.ugatu.su>
2023-05-24 18:28:16 +0300

    Add comments to code

--- scraping/bloomberg.py
+++ scraping/bloomberg.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup as BSoup, Tag
+from bs4.element import PageElement
 from typing import Optional, Tuple
 import pandas as pd
 import re
@@ -6,7 +7,8 @@ import re
 from utils.mongo import MongoData
 
 
-def get_lnk_title(tag: Tag) -> Tuple[Optional[str], Optional[str]]:
+def get_lnk_title(tag: Tag | PageElement) -> Tuple[Optional[str], Optional[str]]:
+    # Extract links and titles from the given tag
     sections = tag.find_all('a', attrs={'href': re.compile(r'https\S*')})
     for section in sections:
         lnk = section.get('href')
@@ -16,37 +18,48 @@ def get_lnk_title(tag: Tag) -> Tuple[Optional[str], Optional[str]]:
     return None, None
 
 
-def get_title_time(tag: Tag) -> str:
+def get_title_time(tag: Tag | PageElement) -> str:
+    # Extract the title time from the given tag
     section = tag.find("div", attrs={"data-component": "recent-timestamp"})
     return section.text if section else ''
 
 
 def bloomberg(page: BSoup) -> pd.DataFrame:
+    # Create an empty DataFrame
     df = pd.DataFrame()
+
+    # Create empty lists to store titles, links, and times
     titles, links, times = [], [], []
+
+    # Specify the sections to extract data from
     sections = [
-        "styles_info__E4gXL",               # Main Headline
-        "styles_storiesContainer__kLMAY",   # Latest News
+        "styles_info__E4gXL",  # Main Headline
+        "styles_storiesContainer__kLMAY",  # Latest News
     ]
 
     for section in sections:
+        # Find all tags with the specified class in the page
         tags: Tag = page.find(class_=section)
         for tag in tags:
             try:
+                # Extract link, title, and title time from the tag
                 link, title = get_lnk_title(tag)
                 title_time = get_title_time(tag)
 
                 if link not in links and link is not None:
+                    # Append unique links, titles, and times to the respective lists
                     links.append(link)
                     titles.append(title)
                     times.append(title_time)
                     print(f'News {title=}')
 
             except Exception as error:
+                # Handle any exceptions that occur during extraction
                 print(repr(error))
 
+    # Assign the lists as columns in the DataFrame
     df[MongoData.Title] = titles
     df[MongoData.Link] = links
     df[MongoData.Time] = times
-    
+
     return df
--- utils/slack.py
+++ utils/slack.py
@@ -5,9 +5,14 @@ SLACK_CHANNEL: str = "test"
 SLACK_BOT_TOKEN: str = "xoxb-5310612778596-5305206526405-PJNrT3HOi0okNo9dLWKnyZMO"
 
 
-def msg_slack(text: str):
+def message_to_slack(text: str):
     try:
+        # Create a WebClient instance using the SLACK_BOT_TOKEN
         client = WebClient(token=SLACK_BOT_TOKEN)
+
+        # Send a message to the specified SLACK_CHANNEL
         client.chat_postMessage(channel="#" + SLACK_CHANNEL, text=text)
+
     except SlackApiError as error:
+        # Handle any Slack API errors that occur
         print(repr(error))
