c915a5ffdeac72d0796ca5eac9451b767d5a0957
Кирилл В Фрейдин <frejdin.kirill-v@net.ugatu.su>
2023-05-24 19:42:16 +0300

    Fix slack token

--- utils/slack.py
+++ utils/slack.py
@@ -1,17 +1,36 @@
+import os
+import json
 from slack_sdk.errors import SlackApiError
 from slack_sdk import WebClient
 
-SLACK_CHANNEL: str = "test"
-SLACK_BOT_TOKEN: str = "xoxb-5310612778596-5305206526405-PJNrT3HOi0okNo9dLWKnyZMO"
+SLACK_BOT_TOKEN = "SLACK_BOT_TOKEN"
 
 
-def message_to_slack(text: str):
+def get_slack_token(file_path) -> str:
+    # This function retrieves the Slack OAuth token from a configuration file
+    with open(file_path, 'r') as config_file:
+        config = json.load(config_file)
+        return config['slack']['oauth_token']
+
+
+def message_to_slack(message: str, channel: str = "test"):
+    # If the message is empty, return without sending a Slack message
+    if message == '':
+        return
+
     try:
-        # Create a WebClient instance using the SLACK_BOT_TOKEN
-        client = WebClient(token=SLACK_BOT_TOKEN)
+        if SLACK_BOT_TOKEN in os.environ:
+            # Retrieve the Slack token from environment variables if available
+            token = os.environ.get(SLACK_BOT_TOKEN)
+        else:
+            # Retrieve the Slack token from a configuration file
+            token = get_slack_token('slack_cfg.json')
 
-        # Send a message to the specified SLACK_CHANNEL
-        client.chat_postMessage(channel="#" + SLACK_CHANNEL, text=text)
+        # Create a WebClient instance using the Slack token
+        client = WebClient(token=token)
+
+        # Send a message to the specified channel
+        client.chat_postMessage(channel="#" + channel, text=message)
 
     except SlackApiError as error:
         # Handle any Slack API errors that occur
