b4c2ad81b779320bb9b50a661650720721141add
Кирилл В Фрейдин <frejdin.kirill-v@net.ugatu.su>
2023-05-29 15:24:58 +0300

    Changes in batch file

--- cronjob.bat
+++ cronjob.bat
@@ -1,26 +1,22 @@
 @echo off
 setlocal
 
-REM Проверяем наличие переменной среды PYTHON_PATH
-echo Checking environment variable PYTHON_PATH
+REM Checking existance of environment variable PYTHON_PATH
 if "%PYTHON_PATH%"=="" (
     echo Environment variable PYTHON_PATH doesn't set.
     exit /b
-) else (
-	echo OK
 )
 
-REM Проверяем существование файла python.exe
-echo Checking python.exe in PYTHON_PATH
+REM Checking python.exe in PYTHON_PATH
 if not exist "%PYTHON_PATH%\python.exe" (
     echo File python.exe couldn't be found in path: %PYTHON_PATH%
     exit /b
-) else (
-	echo OK
 )
 
+REM batch-file folder
+set "batch_folder=%~dp0"
 
-REM Запускаем main.py
-echo Start
-"%PYTHON_PATH%\python.exe" "D:\MyPython WorkFiles\MongoDBProject\main.py"
-echo End
+REM Start main.py
+"%PYTHON_PATH%\python.exe" "%batch_folder%\main.py"
+
+echo Done
